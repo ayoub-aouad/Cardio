179e7af657f9d4797b6800cff7032de707f0544c
ayoub-aouad <ayoub.aouad.1996@gmail.com>
2022-02-10 21:33:08 +0100

    Commiting some other changes as Sitting in the access right

--- hospitalisation/__manifest__.py
+++ hospitalisation/__manifest__.py
@@ -20,6 +20,7 @@
 
     # always loaded
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/partner_inherit.xml',
         'views/osi_diagnostics.xml',
